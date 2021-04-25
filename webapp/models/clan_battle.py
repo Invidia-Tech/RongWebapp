343b9d1a49cb54d7b61cc23c39611338c7ef38f1
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-04-25 12:11:42 +1200

    Fix field name

--- webapp/models/clan_battle.py
+++ webapp/models/clan_battle.py
@@ -3,6 +3,6 @@ from django.db import models
 class ClanBattle(models.Model):
     clan = models.ForeignKey('Clan', on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    game_id = models.ForeignKey('ClanBattleSchedule', null=True, on_delete=models.SET_NULL)
+    game_battle = models.ForeignKey('ClanBattleSchedule', null=True, on_delete=models.SET_NULL)
     begin_time = models.DateTimeField()
     end_time = models.DateTimeField()
