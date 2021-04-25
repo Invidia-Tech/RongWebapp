5b233b03fefe5e7a042791fba6441a89cac301b8
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-04-25 12:47:37 +1200

    Change project folder structure

--- rongdjango/wsgi.py
+++ rongdjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rong project.
+WSGI config for rongdjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rong.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rongdjango.settings')
 
 application = get_wsgi_application()
