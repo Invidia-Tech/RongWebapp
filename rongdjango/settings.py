161327e0fd8217768d1e015ed0adb8759fd8c310
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-04-26 14:39:08 +1200

    Move allowed_hosts to config.py

--- rongdjango/settings.py
+++ rongdjango/settings.py
@@ -20,13 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# taken from config now instead
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# taken from config now instead
-
-ALLOWED_HOSTS = []
+# secret key, debug, allowed hosts taken from config.py directly
 
 
 # Application definition
