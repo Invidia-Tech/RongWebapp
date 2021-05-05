75fd86c405f53f8ec961a50a9e51ef248ec72a71
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-05-05 13:35:14 +1200

    Add bracket to PA comp

--- rong/models/princess_arena_comp.py
+++ rong/models/princess_arena_comp.py
@@ -4,6 +4,7 @@ class PrincessArenaComp(models.Model):
     name = models.CharField(max_length=20)
     user_id = models.PositiveIntegerField(null=True)
     pfp_unit = models.ForeignKey('Unit', on_delete=models.DO_NOTHING)
+    bracket = models.CharField(max_length=20, null=True)
     last_updated = models.DateTimeField()
     team1 = models.OneToOneField('Team', on_delete=models.CASCADE, related_name='team1_for')
     team2 = models.OneToOneField('Team', on_delete=models.CASCADE, related_name='team2_for')
