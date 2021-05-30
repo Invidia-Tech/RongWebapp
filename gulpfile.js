e2b2968537e9ba84a3554471ece549dba0273005
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-05-30 21:04:17 +1200

    Finish sass-ing styles

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+gulp.task('styles', () => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./rong/static/rong/styles/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'rong/static/rong/styles/main.css',
+        'rong/static/rong/styles/pages/*.css',
+    ]);
+});
+
+gulp.task('watch', () => {
+    gulp.watch('sass/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
