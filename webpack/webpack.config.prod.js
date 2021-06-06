dea5d8a93bfe65891ec7d93106b8dac5a8bd3d9e
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-06-06 21:28:42 +1200

    Last few deployment tweaks

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -17,7 +17,7 @@ module.exports = merge(common, {
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         new MiniCssExtractPlugin({
-            filename: 'styles/[name]-[contenthash].css',
+            filename: 'styles/[name]-[contenthash:8].css',
         }),
     ],
     module: {
