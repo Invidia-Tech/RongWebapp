db231ddc634dec6d88f7955212e3e5f904bae273
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-06-07 20:23:07 +1200

    Correct error message

--- rong/views/clanbattle.py
+++ rong/views/clanbattle.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import SuspiciousOperation
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
@@ -11,7 +11,7 @@ from rong.models import Clan, ClanBattle
 def view_battle(request, battle):
     clan_battle = get_object_or_404(ClanBattle, slug=battle)
     if not request.user.can_view(clan_battle):
-        raise SuspiciousOperation()
+        raise PermissionDenied()
     # do something
     return HttpResponse('Pasta')
 
@@ -20,6 +20,6 @@ def view_battle(request, battle):
 def list_battles(request, clan):
     clan = get_object_or_404(Clan, slug=clan)
     if not request.user.can_view(clan):
-        raise SuspiciousOperation()
+        raise PermissionDenied()
     # do something
     return render(request, 'rong/clanbattle/list.html', {"clan": clan})
--- rong/views/manageclan.py
+++ rong/views/manageclan.py
@@ -1,16 +1,15 @@
-from django.core.exceptions import SuspiciousOperation
-from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, render
 
 from rong.decorators import login_required
-from rong.models import Clan, ClanBattle
+from rong.models import Clan
 
 
 @login_required
 def list_members(request, clan):
     clan = get_object_or_404(Clan, slug=clan)
     if not request.user.can_manage(clan):
-        raise SuspiciousOperation()
+        raise PermissionDenied()
     # do something
     clan.sync_members()
     return render(request, 'rong/manageclan/members.html', {"clan": clan})
