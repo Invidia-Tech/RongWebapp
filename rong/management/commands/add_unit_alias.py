5af5d4f084f61bac436afe792bf4b111b50b7a1d
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2021-07-08 19:34:43 +1200

    Fix alias adder to support multiple alias for same chara

--- rong/management/commands/add_unit_alias.py
+++ rong/management/commands/add_unit_alias.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db import connection
 
 from rong.models import ClanBattle, ClanBattleScore, Clan, User, ClanMember, Unit, UnitAlias
 
@@ -13,5 +14,6 @@ class Command(BaseCommand):
         unit = Unit.objects.filter(name__iexact=options['unit_name_or_id']).first()
         if not unit:
             unit = Unit.objects.get(id=options['unit_name_or_id'])
-        UnitAlias(unit_id=unit.id, name=options['alias']).save()
+        with connection.cursor() as cur:
+            cur.execute('INSERT INTO "rongbot"."unit_alias" ("unit_id", "unit_name") VALUES(%s, %s)', (unit.id, options['alias']))
         print("Successfully added alias %s for unit %s [%d]" % (options['alias'], unit.name, unit.id))
