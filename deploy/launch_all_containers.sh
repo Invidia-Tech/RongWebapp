7366fbf775a79964c5fc1080234be04606816c11
ForeverAnApple <kinbd8@gmail.com>
2021-11-26 11:27:05 -0800

    Security issue where secrets are pushed into containers. Now it's out.

--- deploy/launch_all_containers.sh
+++ deploy/launch_all_containers.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker-compose -p "Invidia-Prod" --env-file ./deploy/envs/invidia-env up -d
-docker-compose -p "Valk-Prod" --env-file ./deploy/envs/valk-env up -d
-docker-compose -p "Ethereal-Prod" --env-file ./deploy/envs/ethereal-env up -d
+docker-compose -p "Invidia-Prod" --env-file ~/deploy/envs/invidia-env up -d
+docker-compose -p "Valk-Prod" --env-file ~/deploy/envs/valk-env up -d
+docker-compose -p "Ethereal-Prod" --env-file ~/deploy/envs/ethereal-env up -d
