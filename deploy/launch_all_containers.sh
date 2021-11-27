82e4cef751080992620649e6981ca16a13ba6ec6
ForeverAnApple <kinbd8@gmail.com>
2021-11-26 23:02:09 -0800

    Updated to launch 2 more containers.

--- deploy/launch_all_containers.sh
+++ deploy/launch_all_containers.sh
@@ -2,3 +2,5 @@
 docker-compose -p "Invidia-Prod" --env-file ~/deploy/envs/invidia-env up -d
 docker-compose -p "Valk-Prod" --env-file ~/deploy/envs/valk-env up -d
 docker-compose -p "Ethereal-Prod" --env-file ~/deploy/envs/ethereal-env up -d
+docker-compose -p "PA-Prod" --env-file ~/deploy/envs/pa-env up -d
+docker-compose -p "Midnight-Prod" --env-file ~/deploy/envs/midnight-env up -d
