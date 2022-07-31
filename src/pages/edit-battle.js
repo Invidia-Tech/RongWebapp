7e44de95aa29341ec1f1ff92a046f71781fdd537
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2022-08-01 05:22:02 +1200

    Fix delete button not working for comps

--- src/pages/edit-battle.js
+++ src/pages/edit-battle.js
@@ -39,4 +39,39 @@ page('clan_edit_battle', function () {
             }
         );
     });
+    $(".delete-comp-button").click(function() {
+        let $row = $(this).closest("tr");
+        let url = $(this).attr("data-url");
+        let d = $("<div>Are you sure you want to delete the comp <span class='name'></span>? This cannot be undone.</div>");
+        d.find(".name").html($row.attr("data-name"));
+        d.dialog(
+            {
+                buttons: {
+                    "Yes": function () {
+                        d.dialog('destroy');
+                        show_loading();
+                        $.ajax({
+                            url: url,
+                            beforeSend: function (xhr) {
+                                xhr.setRequestHeader("X-CSRFToken", $('input[name="csrfmiddlewaretoken"]').val());
+                            },
+                            type: 'DELETE',
+                            success: function (data) {
+                                if (data.success) {
+                                    location.reload();
+                                } else {
+                                    make_alert($('#mainContent'), 'danger', 'Comp could not be removed.');
+                                }
+                                hide_loading();
+                            }
+                        });
+                    },
+                    "No": function () {
+                        d.dialog('destroy');
+                    },
+                },
+                modal: true,
+            }
+        );
+    });
 });
